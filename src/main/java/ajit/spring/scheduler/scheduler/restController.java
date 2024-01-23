fcae6c71bbe7453f81026a2c2cf326c584c2858b
ajitchahal <ajitchahal@gmail.com>
2024-01-23 15:46:10 +0100

    add spring boot controller tests

--- src/main/java/ajit/spring/scheduler/scheduler/restController.java
+++ src/main/java/ajit/spring/scheduler/scheduler/restController.java
@@ -1,5 +1,23 @@
 package ajit.spring.scheduler.scheduler;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class restController {
-    
+    @GetMapping
+    public String getSites() {
+
+        return LocalDateTime.now().toString();
+    }
 }
--- src/test/java/ajit/spring/scheduler/scheduler/SchedulerApplicationTests.java
+++ src/test/java/ajit/spring/scheduler/scheduler/SchedulerApplicationTests.java
@@ -1,13 +1,48 @@
 package ajit.spring.scheduler.scheduler;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import lombok.extern.slf4j.Slf4j;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.hamcrest.Matcher;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SchedulerApplication.class)
+@AutoConfigureMockMvc
+// @TestPropertySource(locations =
+// "classpath:application-integrationtest.properties")
+@Slf4j
 class SchedulerApplicationTests {
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void getRoot() throws Exception {
+		mvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(content()
+						.contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+				.andExpect(content()
+						.string(org.hamcrest.Matchers.containsString(String.valueOf(LocalDateTime.now().getYear()))));
+	}
 }
